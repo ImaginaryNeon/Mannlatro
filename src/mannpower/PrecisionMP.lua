350ccb225e1d61b7b64a6bde2d70e66eeaf4c24e
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-04-14 10:34:55 -0500

    Create PrecisionMP.lua

---
+++ src/mannpower/PrecisionMP.lua
@@ -0,0 +1,29 @@
+SMODS.Consumable {
+    key = 'precision',
+    set = 'Mannpower',
+    atlas = 'mannpowerblind',
+    pos = {
+        y = 6
+    },
+    select_card = 'consumeables',
+    config = { extra = { money = 5, money_gain = 3 } },
+    loc_vars = function(self, info_queue, card)
+        if SMODS.last_hand_oneshot then
+            card.ability.extra.money = card.ability.extra.money + card.ability.extra.money_gain
+        end
+        return { vars = { card.ability.extra.money, card.ability.extra.money_gain } }
+    end,
+    use = function(self, card, area, copier)
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.4,
+            func = function()
+                play_sound('timpani')
+                card:juice_up(0.3, 0.5)
+                ease_dollars(card.ability.extra.money, true)
+                return true
+            end
+        }))
+        delay(0.6)
+    end,
+}
