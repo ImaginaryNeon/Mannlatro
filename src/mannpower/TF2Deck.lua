a4220e4e0f3646786956aee9ae0c59b76d37bb3a
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-04-17 15:13:42 -0500

    Update TF2Deck.lua

--- src/mannpower/TF2Deck.lua
+++ src/mannpower/TF2Deck.lua
@@ -2,12 +2,13 @@ SMODS.Back {
     key = 'manndeck',
     atlas = 'deck',
     pos = { x = 0, y = 0 },
+	unlocked = false,
 --    loc_vars = function(self, info_queue, card)
         -- Add money if the last hand scored over the blind requirements
 --        return { vars = { card.ability.extra.money, card.ability.extra.money_gain } }
 --    end,
     calculate = function(self, back, context)
-        if context.ante_end then
+        if context.round_eval and G.GAME.last_blind and G.GAME.last_blind.boss then
       	    G.E_MANAGER:add_event(Event({
 			          trigger = "before",
 			          func = function()
@@ -31,4 +32,16 @@ SMODS.Back {
       	    }))
         end
     end
+	locked_loc_vars = function(self, info_queue, back)
+        return {
+            vars = {
+                localize { type = 'name_text', set = 'Stake', key = 'stake_black' },
+                colours = { get_stake_col(4) }
+            }
+        }
+    end,
+	check_for_unlock = function(self, args)
+        return args.type == 'win_stake' and get_deck_win_stake() >= 4
+    end
 }
+
