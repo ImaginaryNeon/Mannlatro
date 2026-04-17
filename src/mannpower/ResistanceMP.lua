9117fd975e323dc4e349ecb3007749dcfc6f1b4e
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-04-17 10:54:11 -0500

    Create ResistanceMP.lua

---
+++ src/mannpower/ResistanceMP.lua
@@ -0,0 +1,20 @@
+SMODS.Consumable {
+    key = 'resistance',
+    set = 'Mannpower',
+    atlas = 'mannpowercards',
+    pos = {
+        x = 1,
+        y = 0
+    },
+    select_card = 'consumeables',
+    use = function(self, card, area, copier)
+        if G.GAME.blind and not G.GAME.blind.disabled and G.GAME.blind.boss then
+            return {
+                message = 'Boss Disabled!',
+                func = function() -- This is for timing purposes, it runs after the message
+                    G.GAME.blind:disable()
+                end
+            }
+        end
+    end
+}
