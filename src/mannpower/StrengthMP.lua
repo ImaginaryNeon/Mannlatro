61a48f213a90a33b786274d02aefbe2309b953b1
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-04-23 14:57:01 -0500

    Update StrengthMP.lua

--- src/mannpower/StrengthMP.lua
+++ src/mannpower/StrengthMP.lua
@@ -3,58 +3,39 @@ SMODS.Consumable {
     atlas = 'mannpowercards',
     set = 'Mannpower',
     pos = { x = 0, y = 0 },
-    config = { extra = { max_highlighted = 1, ranks = 2 } },
-    loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.max_highlighted, card.ability.extra.ranks } }
-    end,
+	config = {
+		-- This will add a tooltip.
+		mod_conv = "mannpower_strange_seal",
+		-- Tooltip args
+		seal = { mult = 0, change = 1 },
+		max_highlighted = 1,
+	},
+	loc_vars = function(self, info_queue, center)
+		-- Handle creating a tooltip with set args.
+		info_queue[#info_queue + 1] =
+			{ set = "Other", key = "mannpower_strange_seal", specific_vars = { self.config.seal.mult, self.config.seal.change } }
+		return { vars = { center.ability.max_highlighted } }
+	end,
     use = function(self, card, area, copier)
+        local conv_card = G.hand.highlighted[1]
         G.E_MANAGER:add_event(Event({
-            trigger = 'after',
-            delay = 0.4,
             func = function()
                 play_sound('tarot1')
                 card:juice_up(0.3, 0.5)
                 return true
             end
         }))
-        for i = 1, #G.hand.highlighted do
-            local percent = 1.15 - (i - 0.999) / (#G.hand.highlighted - 0.998) * 0.3
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.15,
-                func = function()
-                    G.hand.highlighted[i]:flip()
-                    play_sound('card1', percent)
-                    G.hand.highlighted[i]:juice_up(0.3, 0.3)
-                    return true
-                end
-            }))
-        end
-        delay(0.2)
-        for i = 1, #G.hand.highlighted do
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.1,
-                func = function()
-                    -- SMODS.modify_rank will increment/decrement a given card's rank by a given amount
-                    assert(SMODS.modify_rank(G.hand.highlighted[i], to_number(math.floor(card.ability.extra.ranks))))
-                    return true
-                end
-            }))
-        end
-        for i = 1, #G.hand.highlighted do
-            local percent = 0.85 + (i - 0.999) / (#G.hand.highlighted - 0.998) * 0.3
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.15,
-                func = function()
-                    G.hand.highlighted[i]:flip()
-                    play_sound('tarot2', percent, 0.6)
-                    G.hand.highlighted[i]:juice_up(0.3, 0.3)
-                    return true
-                end
-            }))
-        end
+
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.1,
+            func = function()
+                conv_card:set_seal("mannpower_strange", nil, true)
+                return true
+            end
+        }))
+
+        delay(0.5)
         G.E_MANAGER:add_event(Event({
             trigger = 'after',
             delay = 0.2,
@@ -63,7 +44,6 @@ SMODS.Consumable {
                 return true
             end
         }))
-        delay(0.5)
     end,
     can_use = function(self, card)
         return G.hand and #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.max_highlighted
