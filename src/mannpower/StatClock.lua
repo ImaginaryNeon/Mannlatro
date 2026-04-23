de28a3d47d9d3efc44c81a4f277a33890eb67b90
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-04-23 10:31:11 -0500

    Lose the plot

---
+++ src/mannpower/StatClock.lua
@@ -0,0 +1,28 @@
+SMODS.Seal {
+    key = 'Strange',
+    atlas = 'statclock',
+    pos = { x = 0, y = 0 },
+    config = {
+        extra = {
+            mult = 0,
+            change = 1,
+        }
+    },
+    badge_colour = SMODS.ConsumableTypes['Mannpower'].primary_colour,
+    calculate = function(self, card, context)
+        if context.main_scoring and context.cardarea == G.play then
+            SMODS.scale_card(card, {
+                ref_table = card.ability.extra,
+                ref_value = 'mult',
+                scalar_value = 'change',
+                message_colour = G.C.ATTENTION
+            })
+            return {
+                mult = card.ability.extra.mult
+            }
+        end
+    end,
+    loc_vars = function(self, info_queue, card)
+        return { vars = { self.config.extra.mult, self.config.extra.change } }
+    end
+}
