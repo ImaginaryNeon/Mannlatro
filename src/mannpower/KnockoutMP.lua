02996d158cad2acea36c2ea9d4a26b35f96aa966
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-04-23 14:39:19 -0500

    Create KnockoutMP.lua

---
+++ src/mannpower/KnockoutMP.lua
@@ -0,0 +1,72 @@
+SMODS.Consumable {
+    key = 'knockout',
+    atlas = 'mannpowercards',
+    set = 'Mannpower',
+    pos = { x = 0, y = 2 },
+    config = { extra = { max_highlighted = 2, ranks = 2 } },
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.max_highlighted, card.ability.extra.ranks } }
+    end,
+    use = function(self, card, area, copier)
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.4,
+            func = function()
+                play_sound('tarot1')
+                card:juice_up(0.3, 0.5)
+                return true
+            end
+        }))
+        for i = 1, #G.hand.highlighted do
+            local percent = 1.15 - (i - 0.999) / (#G.hand.highlighted - 0.998) * 0.3
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.15,
+                func = function()
+                    G.hand.highlighted[i]:flip()
+                    play_sound('card1', percent)
+                    G.hand.highlighted[i]:juice_up(0.3, 0.3)
+                    return true
+                end
+            }))
+        end
+        delay(0.2)
+        for i = 1, #G.hand.highlighted do
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.1,
+                func = function()
+                    -- SMODS.modify_rank will increment/decrement a given card's rank by a given amount
+                    assert(SMODS.modify_rank(G.hand.highlighted[i], to_number(math.floor(card.ability.extra.ranks))))
+                    G.hand.highlighted[i]:set_ability(G.P_CENTERS.c_base, true, nil)
+                    return true
+                end
+            }))
+        end
+        for i = 1, #G.hand.highlighted do
+            local percent = 0.85 + (i - 0.999) / (#G.hand.highlighted - 0.998) * 0.3
+            G.E_MANAGER:add_event(Event({
+                trigger = 'after',
+                delay = 0.15,
+                func = function()
+                    G.hand.highlighted[i]:flip()
+                    play_sound('tarot2', percent, 0.6)
+                    G.hand.highlighted[i]:juice_up(0.3, 0.3)
+                    return true
+                end
+            }))
+        end
+        G.E_MANAGER:add_event(Event({
+            trigger = 'after',
+            delay = 0.2,
+            func = function()
+                G.hand:unhighlight_all()
+                return true
+            end
+        }))
+        delay(0.5)
+    end,
+    can_use = function(self, card)
+        return G.hand and #G.hand.highlighted > 0 and #G.hand.highlighted <= card.ability.extra.max_highlighted
+    end
+}
