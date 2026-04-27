a3491f40a237f82d20904e49c0b80a947eaede5f
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-04-27 09:55:51 -0500

    Create Regen.lua

---
+++ src/blinds/Regen.lua
@@ -0,0 +1,42 @@
+SMODS.Blind {
+    key = "regen",
+    atlas = 'mannpowerblind',
+    pos = {
+        y = 5
+    },
+    boss = { min = 4 },
+  	config = {
+  		blind_mod = 0.25,
+  	},
+    loc_vars = function(self, info_queue, card)
+	  	return {
+		  	vars = {
+			  	number_format(lenient_bignum((get_blind_amount(G.GAME.round_resets.ante) * 0.25))),
+			  },
+	  	}
+  	end,
+  	collection_loc_vars = function(self)
+	  	return {
+		  	vars = {
+			  	"(" .. number_format(0.25) .. "X base)",
+  			},
+	  	}
+	  end,
+    boss_colour = HEX("ED712B"),
+    calculate = function(self, blind, context)
+        if not blind.disabled then
+            if context.after then
+                return {
+                    score = get_blind_amount(G.GAME.round_resets.ante) * -0.25,
+                }
+                if G.GAME.chips < 0 then
+                return {
+                    score = G.GAME.chips * -1,
+                }
+                end
+            end
+        end
+    end
+--		G.GAME.blind.chip_text = number_format(G.GAME.blind.chips)
+	end,
+}
