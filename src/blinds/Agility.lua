5af4b3d790d3374b62bb95e849d71cfbec10baa2
ImaginaryNeon <92883596+ImaginaryNeon@users.noreply.github.com>
2026-05-11 19:19:46 -0500

    Update Agility.lua

--- src/blinds/Agility.lua
+++ src/blinds/Agility.lua
@@ -5,7 +5,7 @@ function Game.update(self, dt)
     if not G.SETTINGS.paused and blind and blind.config.blind.key == 'bl_mannpower_agility' then
         blind.effect.extra.timer = blind.effect.extra.timer + dt
         -- trigger after 10 seconds
-        if blind.effect.extra.timer >= 8 then
+        if blind.effect.extra.timer >= 8 and not blind.debuffed then
             -- reset timer to 0
             blind.effect.extra.timer = 0
             -- do your effect that happens every 10 seconds here (thanks to srockw for helping with this)
